f49d49cb073234306c5125fd97cdffc166539edb
wylew <wy@ttlew.in>
2020-02-12 18:43:25 -0800

    Update degrees.js

--- app/data/resume/degrees.js
+++ app/data/resume/degrees.js
@@ -1,15 +1,15 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  }, {
+    school: 'University of Puget Sound',
+    degree: 'B.A. English, Creative Writing',
+    link: 'https://ups.edu',
+    year: 2006,
+  }, /* {
     school: 'University at Buffalo',
     degree: 'B.S. Electrical Engineering, Computer Engineering',
     link: 'https://buffalo.edu',
     year: 2012,
-  },
+  }, */
 ];
 
 export default degrees;
