422b1e6a48760d1a556a8ad780b559c6b78c8e62
wylew <wy@ttlew.in>
2020-02-12 21:39:19 -0800

    Update positions.js

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -46,10 +46,8 @@ const positions = [
       'Received Series A term sheets for 20M+ valuations.',
     ],
   }, {
-    company: 'Planet',
     position: 'Missions Intern',
-    link: 'https://planet.com',
-    daterange: 'June 2014 - January 2015',
+    daterange: 'September 2012 - July 2014',
     points: [
       'Built models to improve image quality, signal to noise ratio, and dynamic range.',
       'Performed statistical analysis of image quality in Matlab and Python.  Developed flight software in C++.',
